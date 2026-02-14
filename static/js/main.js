31f267f50dfcc3ad5aaa409be6b4df3e2f7f92d3
SabaBugi <saba.boogy@gmail.com>
2026-02-14 15:33:47 +0400

    mobile integration update

--- static/js/main.js
+++ static/js/main.js
@@ -67,3 +67,17 @@ document.addEventListener("DOMContentLoaded", () => {
   window.addEventListener("scroll", revealOnScroll);
   revealOnScroll(); // trigger on page load
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+
+  const hamburger = document.getElementById("hamburger");
+  const navMenu = document.getElementById("nav-menu");
+
+  hamburger.addEventListener("click", function () {
+    navMenu.classList.toggle("active");
+    hamburger.classList.toggle("active");
+  });
+
+});
+
+
